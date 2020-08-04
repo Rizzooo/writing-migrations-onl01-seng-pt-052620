ba353e20519b8985db6240ee3f1fabb9775dfc92
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-04 15:33:36 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-  def change_column(:students, :birthdate, :integer)
+  def change_column
     change_column :students, :birthdate, :datetime
   end
 end
